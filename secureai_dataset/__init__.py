dcd1da2f4a361d23c53eabda79e7945e8956471d
amarrr33 <109675040+amarrr33@users.noreply.github.com>
2025-10-15 23:05:52 +0530

    Delete secureai_dataset directory

--- secureai_dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-"""SecureAI dataset package (minimal)."""
-__all__ = [
-    "setup_complete_dataset",
-    "anamorpher_integration",
-    "benign_collector",
-    "cyberseceval3_processor",
-    "evaluation_framework",
-]
--- secureai_dataset/cyberseceval3_processor.py
+++
@@ -1,9 +0,0 @@
-"""Placeholder processor for CyberSecEval3 dataset.
-
-This module intentionally does not download external data. It contains helpers
-to integrate the real dataset later.
-"""
-
-def check_availability():
-    return False
-
